77fdc7ff902fbee82837c1374858e1b8e499a547
daniel <danielstmontoya@gmail.com>
2024-07-28 09:49:54 -0500

    replace double quotes to single quotes

--- functions.py
+++ functions.py
@@ -1,71 +1,75 @@
-"""Este archivo devuelve las funciones correspondientes para la API"""
+'''Este archivo devuelve las funciones correspondientes para la API'''
 
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-movies = pd.read_csv("Machine Learning Model/movies_dataset_cleaned.csv")
-final_data_set = pd.read_csv("Machine Learning Model/final_movie_set.csv",compression="gzip")
-actors = pd.read_csv("Machine Learning Model/actors.csv",compression="gzip")
-crew = pd.read_csv("Machine Learning Model/crew.csv",compression="gzip")
+movies = pd.read_csv('Machine Learning Model/movies_dataset_cleaned.csv')
+final_data_set = pd.read_csv('Machine Learning Model/final_movie_set.csv',compression='gzip')
+actors = pd.read_csv('Machine Learning Model/actors.csv',compression='gzip')
+crew = pd.read_csv('Machine Learning Model/crew.csv',compression='gzip')
 
 def cantidad_filmaciones_mes(mes):
   meses = {
-        "enero":1,
-        "febrero":2,
-        "marzo":3,
-        "abril":4,
-        "mayo":5,
-        "junio":6,
-        "julio":7,
-        "agosto":8,
-        "septiembre":9,
-        "octubre":10,
-        "noviembre":11,
-        "diciembre":12
+        'enero':1,
+        'febrero':2,
+        'marzo':3,
+        'abril':4,
+        'mayo':5,
+        'junio':6,
+        'julio':7,
+        'agosto':8,
+        'septiembre':9,
+        'octubre':10,
+        'noviembre':11,
+        'diciembre':12
     }
   valor = meses[mes]
-  movies["release_date"] = pd.to_datetime(movies['release_date'], errors='coerce')
-  return movies[movies["release_date"].dt.month == valor]["release_date"].count()
+  movies['release_date'] = pd.to_datetime(movies['release_date'], errors='coerce')
+  return movies[movies['release_date'].dt.month == valor]['release_date'].count()
 
 
 def cantidad_filmaciones_dia(dia):
   dias = {
-    "lunes":"Monday",
-    "martes":"Tuesday",
-    "miercoles":"Wednesday",
-    "jueves":"Thursday",
-    "viernes":"Friday",
-    "sabado":"Saturday",
-    "domingo":"Sunday"
+    'lunes':'Monday',
+    'martes':'Tuesday',
+    'miercoles':'Wednesday',
+    'jueves':'Thursday',
+    'viernes':'Friday',
+    'sabado':'Saturday',
+    'domingo':'Sunday'
   }
   valor = dias[dia]
-  movies["release_date"] = pd.to_datetime(movies['release_date'], errors='coerce')
-  return movies[movies["release_date"].dt.strftime('%A') == valor]["release_date"].count()
+  movies['release_date'] = pd.to_datetime(movies['release_date'], errors='coerce')
+  return movies[movies['release_date'].dt.strftime('%A') == valor]['release_date'].count()
 
-def total_votos(titulo=""):
+def total_votos(titulo=''):
   titulo = titulo.title()
-  movies["title"] = movies["title"].str.title()
-  movie_to_return = movies.iloc[movies.index[movies["title"] == titulo]]
-  if movie_to_return["vote_count"].iloc[0] >= 2000:
-    return {"message": f"La pelicula {movie_to_return["title"].iloc[0]} fue estrenada el año {movie_to_return["release_year"].iloc[0]}. La misma cuenta con un total de {movie_to_return["vote_count"].iloc[0].astype(int)} valoraciones, con un promedio de {movie_to_return["vote_average"].iloc[0].round(2)}"}
+  movies['title'] = movies['title'].str.title()
+  movie_to_return = movies.iloc[movies.index[movies['title'] == titulo]]
+  if movie_to_return['vote_count'].iloc[0] >= 2000:
+    return {'message': f'La pelicula {movie_to_return['title'].iloc[0]} fue estrenada el año {movie_to_return['release_year'].iloc[0]}. La misma cuenta con un total de {movie_to_return['vote_count'].iloc[0].astype(int)} valoraciones, con un promedio de {movie_to_return['vote_average'].iloc[0].round(2)}'}
+  else:
+    return {'message':'La pelicula no cuenta con la cantidad de votaciones requerida para mostrar la informacion'}
+print(total_votos('Riddick'))
 
-def score_titulo(titulo=""):
+def score_titulo(titulo=''):
   titulo = titulo.title()
-  movies["title"] = movies["title"].str.title()
-  movie_to_return = movies.iloc[movies.index[movies["title"]==titulo]]
-  return {"message": f"La pelicula {movie_to_return["title"].iloc[0]} fue estrenada el año {movie_to_return["release_year"].iloc[0]} con un score/popularidad de {movie_to_return["popularity"].iloc[0].round(2)}"}
+  movies['title'] = movies['title'].str.title()
+  movie_to_return = movies.iloc[movies.index[movies['title']==titulo]]
+  return {'message': f'La pelicula {movie_to_return['title'].iloc[0]} fue estrenada el año {movie_to_return['release_year'].iloc[0]} con un score/popularidad de {movie_to_return['popularity'].iloc[0].round(2)}'}
 
-def get_actor(actor=""):
+
+def get_actor(actor=''):
   actor = actor.title()
-  revenue = movies[["id","revenue"]]
-  revenue_actor = actors[["id","actor_name"]].join(revenue.set_index("id"),on="id",how="left")
-  return_movies = revenue_actor[revenue_actor["actor_name"]==actor]["revenue"].sum()
-  total_movies = revenue_actor[revenue_actor["actor_name"]==actor]["revenue"].count()
-  return_avg = revenue_actor[revenue_actor["actor_name"]==actor]["revenue"].mean()
-  return {"message":f"{actor} ha participado de {total_movies} cantidad de filmaciones. Ha conseguido un retorno de {'${:,.2f}'.format(return_movies)} con un promedio de {'${:,.2f}'.format(return_avg)} por filmación"}
+  revenue = movies[['id','revenue']]
+  revenue_actor = actors[['id','actor_name']].join(revenue.set_index('id'),on='id',how='left')
+  return_movies = revenue_actor[revenue_actor['actor_name']==actor]['revenue'].sum()
+  total_movies = revenue_actor[revenue_actor['actor_name']==actor]['revenue'].count()
+  return_avg = revenue_actor[revenue_actor['actor_name']==actor]['revenue'].mean()
+  return {'message':f'{actor} ha participado de {total_movies} cantidad de filmaciones. Ha conseguido un retorno de {'${:,.2f}'.format(return_movies)} con un promedio de {'${:,.2f}'.format(return_avg)} por filmación'}
 
-def get_director(director=""):
-  movie_costs = movies[["id","title","budget","revenue","release_date"]].set_index("id")
-  directors = crew[(crew["crew_job"]=="Director")&(crew["crew_name"]==director)]
-  directors_movies = directors.join(movie_costs,on="id",how="left")
-  return directors_movies[["id","title","crew_name","crew_job","revenue","budget","release_date"]].rename(columns={"crew_name":"director_name","crew_job":"job"}).to_dict(orient="records")
+def get_director(director=''):
+  movie_costs = movies[['id','title','budget','revenue','release_date']].set_index('id')
+  directors = crew[(crew['crew_job']=='Director')&(crew['crew_name']==director)]
+  directors_movies = directors.join(movie_costs,on='id',how='left')
+  return directors_movies[['id','title','crew_name','crew_job','revenue','budget','release_date']].rename(columns={'crew_name':'director_name','crew_job':'job'}).to_dict(orient='records')
--- main.py
+++ main.py
@@ -5,34 +5,34 @@ import functions
 app = FastAPI()
 
 
-@app.get("/home")
+@app.get('/home')
 async def root():
-    return {"message": "Welcome to my Machine Engineering Project"}
+    return {'message': 'Welcome to my Machine Engineering Project'}
 
-@app.get("/items/{item_id}")
+@app.get('/items/{item_id}')
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {'item_id': item_id, 'q': q}
 
-@app.get("/cantidad_filmaciones_mes/{mes}")
+@app.get('/cantidad_filmaciones_mes/{mes}')
 def cantidad_filmaciones_mes(mes: str):
-    return {"message": f"{functions.cantidad_filmaciones_mes(mes)} cantidad de peliculas que fueron estrenadas el mes de {mes.title()}"}
+    return {'message': f'{functions.cantidad_filmaciones_mes(mes)} cantidad de peliculas que fueron estrenadas el mes de {mes.title()}'}
 
-@app.get("/cantidad_filmaciones_dia/{dia}")
+@app.get('/cantidad_filmaciones_dia/{dia}')
 def cantidad_filmaciones_dia(dia: str):
-    return {"message":f"{functions.cantidad_filmaciones_dia(dia)} cantidad de peliculas que fueron estrenadas los dias {dia.title()}"}
+    return {'message':f'{functions.cantidad_filmaciones_dia(dia)} cantidad de peliculas que fueron estrenadas los dias {dia.title()}'}
 
-@app.get("/score_titulo/{titulo}")
+@app.get('/score_titulo/{titulo}')
 def score_titulo(titulo):
     return functions.score_titulo(titulo)
 
-@app.get("/votos_titulo/{titulo}")
+@app.get('/votos_titulo/{titulo}')
 def votos_titulo(titulo):
     return functions.total_votos(titulo)
 
-@app.get("/get_actor/{actor}")
-def get_actor(actor=""):
+@app.get('/get_actor/{actor}')
+def get_actor(actor=''):
     return functions.get_actor(actor)
 
-@app.get("/get_director/{director}")
-def get_director(director=""):
+@app.get('/get_director/{director}')
+def get_director(director=''):
     return functions.get_director(director)
