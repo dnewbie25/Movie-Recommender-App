11f58cd7bc0a4ec30cdee085a99c7c093579966e
daniel <danielstmontoya@gmail.com>
2024-09-01 13:15:35 -0500

    API basic landing page for interacting completed

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from typing import Optional
-from fastapi import FastAPI
+# from typing import Optional
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import functions
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/home')
-async def root():
-    return {'message': 'Welcome to my Data Engineering Project!! Please enter https://movie-recommender-app-ejx7.onrender.com/docs to see the full API'}
+@app.get('/')
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
 
 @app.get('/cantidad_filmaciones_mes/{mes}')
-def cantidad_filmaciones_mes(mes: str):
+async def cantidad_filmaciones_mes(mes: str):
     return {'message': f'{functions.cantidad_filmaciones_mes(mes)} cantidad de peliculas que fueron estrenadas el mes de {mes.title()}'}
+    # return templates.TemplateResponse("mes.html", {"request": request, "response_data": response_data})
 
 @app.get('/cantidad_filmaciones_dia/{dia}')
 def cantidad_filmaciones_dia(dia: str):
@@ -34,7 +37,8 @@ def get_director(director=''):
     return functions.get_director(director)
 
 
-# the ML model is using more than the allocated RAM, need to optimize
-# @app.get('/movie_recommender/{titulo}')
-# def movie_recommender(titulo=''):
-#     return functions.recommend_movie(titulo)
+# --->> the ML model is using more than the allocated RAM, need to optimize
+
+@app.get('/movie_recommender/{titulo}')
+def movie_recommender(titulo=''):
+    return functions.recommend_movie(titulo)
